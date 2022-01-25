b94bc03f6ab355e035daaf06310e0345c30b4b2c
Adriana <hrl.davidson@gmail.com>
2022-01-25 12:30:10 +0100

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -1,24 +1,24 @@
 
 /***** Erinnerung: Arrays *******/
 
-let arr;
-arr = ["Ich","bin","Max"];
-output(arr);
-output(arr[0]);
-output("------------");
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
 
-// nested arrays
-arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
-output(arr[0][2]);
-output(arr[1][2]);
-output("------------");
+// // nested arrays
+// arr = [["Ich","bin","Max"],["Ich","bin","Moritz"]];
+// output(arr[0][2]);
+// output(arr[1][2]);
+// output("------------");
 
-// nested loops
-for (let i = 0; i < arr.length; i++) {
-       for (let j = 0; j < arr[0].length; j++) { 
-        output(arr[i][j]);          
-       }   
-}
+// // nested loops
+// for (let i = 0; i < arr.length; i++) {
+//        for (let j = 0; j < arr[0].length; j++) { 
+//         output(arr[i][j]);          
+//        }   
+// }
 
 
 
@@ -26,7 +26,30 @@ for (let i = 0; i < arr.length; i++) {
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = 
+        {
+                firstName:"Tylo",
+                familyName:"Schoenemeyer",
+                salary:[1200,1600],
+                permission: true,
+                sayHello: 
+                        function() {
+                        return "Hello, ich bin " + person.firstName;
+                }
+        };
 
+
+output(person);
+person.firstName = "Thilo";
+output(person.firstName);
+output(person.familyName);
+
+const txt = "Ich bin " + person.firstName + " " + 
+            person.familyName + " und verdiene " + 
+            person.salary[1] + " p.a."
+output(txt);
+
+output(person.sayHello());
 
 
 /** Ausgabe */
